c5595a533a6765e0e39abf25eeef44a88304b675
shake@home <shake822@163.com>
2014-08-26 23:57:56 +0800

    auto

--- src/com/luoyi/android/apidemo/MainActivity.java
+++ src/com/luoyi/android/apidemo/MainActivity.java
@@ -1,35 +1,123 @@
 package com.luoyi.android.apidemo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
+
+	private static final String INTENT_PATH = "intent_path";
+
+	private static final String LABEL_SPLITE = "/";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		getActivityList();
+		Intent intent = getIntent();
+		String path = intent.getStringExtra(INTENT_PATH);
+		if(path !=null && path.length()>0){
+			setTitle(path);
+			getActionBar().setHomeButtonEnabled(true);
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+		setListAdapter(new SimpleAdapter(this, getActivityList(path),
+				R.layout.activity_list_1, new String[] { "title" },
+				new int[] { R.id.tv_title }));
 	}
-
-	private ArrayList getActivityList() {
-		ArrayList<Map> lst = new ArrayList<Map>();
+	
+	/**
+	 * 获取列表的数据
+	 * @param prefix
+	 * @return
+	 */
+	private ArrayList<HashMap<String, Object>> getActivityList(String prefix) {
+		ArrayList<HashMap<String, Object>> lst = new ArrayList<HashMap<String, Object>>();
 		Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-		mainIntent.addCategory(Intent.CATEGORY_SAMPLE_CODE);
+		mainIntent.addCategory("android.intent.category.shake");
 		PackageManager pm = getPackageManager();
 		List<ResolveInfo> lstAll = pm.queryIntentActivities(mainIntent, 0);
-		System.out.println(lstAll);
+		String[] prefixPath;
+		if (prefix == null || prefix.length() == 0) {
+			prefixPath = null;
+		} else {
+			prefixPath = prefix.split(LABEL_SPLITE);
+		}
 		for (ResolveInfo resolveInfo : lstAll) {
-			System.out.println(resolveInfo.resolvePackageName + " "
-					+ resolveInfo);
+			CharSequence labelSeq = resolveInfo.loadLabel(pm);
+			String label = labelSeq == null ? resolveInfo.activityInfo.name
+					: labelSeq.toString();
+			String[] labelPath = label.split(LABEL_SPLITE);
+			String nextLabel = prefixPath == null ? labelPath[0]
+					: labelPath[prefixPath.length];
+			if ((prefixPath != null ? prefixPath.length : 0) == labelPath.length - 1) {
+				// 真实的Activity
+				addItem(lst,
+						nextLabel,
+						activityIntent(resolveInfo.activityInfo.packageName,
+								resolveInfo.activityInfo.name));
+			} else {
+				addItem(lst, nextLabel,
+						browseIntent(prefixPath == null ? nextLabel : prefix
+								+ LABEL_SPLITE + nextLabel));
+			}
 		}
 		return lst;
+	}
+
+	/**
+	 * 添加列表数据
+	 * @param data
+	 * @param name
+	 * @param intent
+	 */
+	protected void addItem(ArrayList<HashMap<String, Object>> data,
+			String name, Intent intent) {
+		HashMap<String, Object> temp = new HashMap<String, Object>();
+		temp.put("title", name);
+		temp.put("intent", intent);
+		data.add(temp);
+	}
+
+	/**
+	 * 真实的
+	 * 
+	 * @param pkg
+	 * @param componentName
+	 * @return
+	 */
+ 
+	protected Intent activityIntent(String pkg, String componentName) {
+		Intent result = new Intent();
+		result.setClassName(pkg, componentName);
+		return result;
+	}
+
+	/**
+	 * 显示列表的
+	 * 
+	 * @param path
+	 * @return
+	 */
+	protected Intent browseIntent(String path) {
+		Intent result = new Intent();
+		result.setClass(this, MainActivity.class);
+		result.putExtra(INTENT_PATH, path);
+		return result;
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		HashMap<String,Object> map =  (HashMap<String,Object> ) l.getItemAtPosition(position);
+		Intent intent = (Intent) map.get("intent");
+		startActivity(intent);
 	}
 }
